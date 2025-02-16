c5d573ab954b80ccf9868ca4ad8a24c9d1f726b2
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2025-02-16 16:50:31 +0530

    connect to mongo ATLAS

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+// Load environment variables from .env file in the root directory
+dotenv.config({
+    path: "./.env" // Ensure the correct path
+});
+
+// Connect to the database
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try{
+         await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         app.on("error",(error)=>{
+            console.log("ERRR:",error);
+            throw error
+         })
+
+         app.listen(process.env.PORT, ()=>{
+            console.log(`App is listeing on port ${process.env.PORT}`);
+         })
+    } catch (error){
+        console.log("ERROR:", error)
+        throw err
+    }
+})() 
+    */
